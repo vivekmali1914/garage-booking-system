38065dcc1e79f3637a16e303402e751721adfef7
vivekmali1914 <malivivek668@gmail.com>
2026-04-25 01:47:10 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,18 +1,7 @@
 const mysql = require("mysql2");
 
-const db = mysql.createPool({
-    host: process.env.MYSQLHOST,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
-    port: Number(process.env.MYSQLPORT),
+const db = mysql.createPool(process.env.MYSQL_URL);
 
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
-});
-
-// Test connection
 db.getConnection((err, connection) => {
     if (err) {
         console.log("Database Error ❌", err);
