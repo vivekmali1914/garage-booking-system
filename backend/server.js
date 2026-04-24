dd9264aa43307da706b864993df850db2d99a3d4
vivekmali1914 <malivivek668@gmail.com>
2026-04-24 23:12:05 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
+});
+
 // TEST API
 app.get("/", (req, res) => {
     res.send("Server Running ✅");
